1fc97cb6b050e2228fe5b54fb2cf37c9c87ce8b8
Mike Flot <mike.flot@base2s.com>
2020-08-07 02:37:09 -0700

    remove like test

--- posts/test_models.py
+++ posts/test_models.py
@@ -8,13 +8,16 @@ class  ModelTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='test', email='test@gmail.com', password='top_secret')
         self.body = 'my example post'
-        self.post = Post(body=self.body, owner=self.user)
-        self.posts = baker.make("posts.Post", _quantity=2)
+        self.posts = baker.make("posts.Post", _quantity=8)
+        self.test_post = self.posts[1]
+        self.test_post.liked.add(self.user)
+
 
     def test_model_user_can_create_a_post(self):
         """Test the single model can create a transaction."""
         old_count = Post.objects.count()
-        self.post.save()
+        post = Post(body=self.body, owner=self.user)
+        post.save()
         new_count = Post.objects.count()
         self.assertNotEqual(old_count, new_count)
 
@@ -26,3 +29,12 @@ class  ModelTestCase(TestCase):
         new_count = post.liked.count()
         self.assertNotEqual(old_count, new_count)
         self.assertTrue(self.user in post.liked.all())
+
+    def test_model_user_can_unlike_post(self):
+        """Test that a user can like a post."""
+        post = self.test_post
+        old_count = post.liked.count()
+        post.liked.remove(self.user)
+        new_count = post.liked.count()
+        self.assertNotEqual(old_count, new_count)
+        self.assertFalse(self.user in post.liked.all())
