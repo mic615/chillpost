bca20f49dd4ec279478aa7809bbb77aab1a92d1c
Mike Flot <mike.flot@base2s.com>
2020-08-06 21:38:41 -0700

    user can create posts

---
+++ posts/api.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, permissions
+from .serializers import PostSerializer
+
+# Post Viewset
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [
+        permissions.IsAuthenticated
+    ]
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        return self.request.user.posts.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    owner = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, blank=True, related_name="liked_posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def toggle_like(self, user):
+        #  liked
+        if user in self.liked.all():
+            self.liked.remove(user)
+        # not liked
+        else:
+            self.liked.add(user)
